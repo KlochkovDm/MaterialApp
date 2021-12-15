6522ea0da1808fda932d62a6e2f375bf51c3937f
Dmitry <klochkov.dm595@yandex.ru>
2021-12-15 23:37:44 +0300

    Init commit + фрагемент и байдинги

---
+++ app/src/main/java/com/example/materialapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.materialapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.materialapp.databinding.MainActivityBinding
+import com.example.materialapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: MainActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(binding.container.id, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
